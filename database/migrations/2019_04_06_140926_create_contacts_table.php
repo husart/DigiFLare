5766731239901718dbed0b7b2912f1a4bf619b95
unknown <mihai_husar1990@yahoo.com>
2019-04-06 18:32:39 +0300

    Added email to contacts migration

--- database/migrations/2019_04_06_140926_create_contacts_table.php
+++ database/migrations/2019_04_06_140926_create_contacts_table.php
@@ -18,7 +18,8 @@ class CreateContactsTable extends Migration
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('name');
-            $table->string('phone')->unique();
+            $table->string('email');
+            $table->string('phone');
             $table->string('address');
             $table->timestamps();
         });
