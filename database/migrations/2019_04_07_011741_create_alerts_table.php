b887ab33055bae7d62a94b6709515a637d7f79af
unknown <mihai_husar1990@yahoo.com>
2019-04-07 04:30:02 +0300

    Migration for the creation of the alerts table

---
+++ database/migrations/2019_04_07_011741_create_alerts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlertsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alerts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('device_id')->unsigned();
+            $table->foreign('device_id')->references('device_id')->on('users');
+            $table->double('latitude', 10, 8);
+            $table->double('longitude', 11, 8);
+            $table->string('status', 3);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alerts');
+    }
+}
